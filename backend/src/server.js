2c27052024284940dcc1e8774037f3b162075ad1
AMRIT <amrit.raj2023@vitstudent.ac.in>
2026-02-05 21:01:20 +0530

    Add backend server setup and environment configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,42 @@
 import express from 'express'
 import { ENV } from './lib/env.js'
-
+import path from "path"
 
 const app = express()
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve()
 
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
     res
     .status(200)
-    .json({msg:"success from api"})
+    .json({msg:"success from api"});
+});
+
+app.get("/books", (req, res) => {
+    res
+    .status(200)
+    .json({msg:"This is the book's endpoints"});
 })
 
-app.listen(3000, () => console.log("Server is running on port", ENV.PORT))
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
+
+//make our app ready for deployment
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
+
+
+const PORT = ENV.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+});
