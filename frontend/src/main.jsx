ca1a0d01787ecd758c040cb831efa1d340664e2d
AMRIT <amrit.raj2023@vitstudent.ac.in>
2026-02-08 15:56:00 +0530

    Check Clerk Key

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,9 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+console.log("CLERK KEY:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+
+
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
