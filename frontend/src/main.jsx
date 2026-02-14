9a07ef3ff9dfa602822b50fff8992d15e015bdd5
AMRIT <amrit.raj2023@vitstudent.ac.in>
2026-02-14 09:10:16 +0530

    feat: update frontend dependencies and implement routing with react-router
    - Added new dependencies: @tailwindcss/vite, @tanstack/react-query, axios, framer-motion, react-hot-toast, react-router, and daisyui.
    - Removed App.css and migrated styles to Tailwind CSS.
    - Refactored App.jsx to implement routing and user authentication with Clerk.
    - Created HomePage and ProblemPage components for routing.
    - Set up axios instance for API calls with credentials.
    - Updated main.jsx to include QueryClientProvider for react-query.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { BrowserRouter } from "react-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -13,10 +17,17 @@ console.log("CLERK KEY:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
+
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <QueryClientProvider client = {queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 );
