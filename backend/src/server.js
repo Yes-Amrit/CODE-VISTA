6900da936b45a7daff02bf7cc61a9802ead4558e
AMRIT <amrit.raj2023@vitstudent.ac.in>
2026-02-20 13:27:15 +0530

    added authorization

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,11 +15,12 @@ const app = express();
 
 const __dirname = path.resolve();
 
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true,allowedHeaders: ["Content-Type", "Authorization"] }));
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
